429ce19191092bc1b1a818236329a25bd3ac8fcc
Konstantin Safarov <safarov-konstantin@yandex.ru>
2023-10-18 22:36:30 +0300

    изменено:      src/phone.py

--- src/phone.py
+++ src/phone.py
@@ -5,17 +5,13 @@ class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
         self.__name = name
-        self.__number_of_sim = number_of_sim
+        self.number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"Phone('{self.__name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     def __str__(self):
         return f'{self.__name}'
-
-    def __add__(self, other):
-        if issubclass(other.__class__, self.__class__):
-            return self.quantity + other.quantity
 
     @property
     def name(self):
